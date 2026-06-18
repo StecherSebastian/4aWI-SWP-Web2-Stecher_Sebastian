3c3ad64eded21037eceecfe6a6164d9aeb82a15e
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2026-06-18 15:08:48 +0200

    Added Unit Tests for Sidebar

--- schoolmanagement/src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ schoolmanagement/src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,1 +1,55 @@
-describe('')
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
+import { Sidebar } from './sidebar.component';
+
+describe('Sidebar', () => {
+    let component: Sidebar;
+    let fixture: ComponentFixture<Sidebar>
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                provideRouter([])
+            ]
+        });
+
+        fixture = TestBed.createComponent(Sidebar);
+        component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render navigation links', () => {
+        const text = fixture.nativeElement.textContent;
+
+        expect(text).toContain('Home');
+        expect(text).toContain('Schools');
+        expect(text).toContain('Classrooms');
+        expect(text).toContain('Students');
+        expect(text).toContain('Create');
+    });
+
+
+    it('should configure correct routes', () => {
+        const links = fixture.debugElement.queryAll(
+            By.directive(RouterLink)
+        );
+
+        const routes = links.map(
+            link => link.injector.get(RouterLink).routerLink
+        );
+
+        expect(routes).toEqual([
+            '/dashboard',
+            '/dashboard',
+            '/schools',
+            '/classrooms',
+            '/students',
+            '/create'
+        ]);
+    });
+});
--- schoolmanagement/src/app/shared/components/sidebar/sidebar.component.ts
+++ schoolmanagement/src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,4 +7,4 @@ import { RouterLink } from "@angular/router";
     styleUrl: 'sidebar.component.css',
     imports: [RouterLink]
 })
-export class Sidbar {};
+export class Sidebar {};
